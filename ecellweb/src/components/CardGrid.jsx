f620740c2a9599de28e76916376f88c477d60814
Vyom <vyom262327@gmail.com>
2025-08-20 14:34:07 +0530

    stuff

--- ecellweb/src/components/CardGrid.jsx
+++ ecellweb/src/components/CardGrid.jsx
@@ -74,28 +74,6 @@ const CardGrid = () => {
               <p className="mb-3 font-normal text-gray-300">{description}</p>
             </div>
 
-            {/* Read More Button */}
-            <a
-              href={url}
-              className="inline-flex items-center px-4 py-2 text-sm font-medium text-orange-400 bg-stone-800 rounded-lg hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-orange-500"
-            >
-              Read more
-              <svg
-                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
           </div>
         ))}
       </div>
